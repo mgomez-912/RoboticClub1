a6482127efa02ea752e4355ffe37a6648f5c284f
mgomez-912 <gomez.val95@outlook.com>
2025-04-09 23:34:27 +0800

    V1.2 Motor test added

--- include/TaskTest.h
+++ include/TaskTest.h
@@ -4,8 +4,14 @@
 #include <Arduino.h>
 #include "GlobalVariables.h"
 
-const int test = LED_BUILTIN;      
+const int test = LED_BUILTIN;
+const int M1F = 35;  
+const int M1R = 36;
+const int M2F = 37;
+const int M2R = 38;    
 
 void taskTest(void *pvParameters);  // Declaration of task function
+
+void stopMotors();
 
 #endif
--- src/TaskTest.cpp
+++ src/TaskTest.cpp
@@ -4,12 +4,31 @@ void taskTest(void *pvParameters) {
     pinMode(test, OUTPUT);
     
     while(true){
-        digitalWrite(test, HIGH);
-        delay(500);
-        digitalWrite(test, LOW);
-        delay(500);
+        // digitalWrite(motorTest, HIGH);
+        // delay(500);
+        // digitalWrite(motorTest, LOW);
+        // delay(500);
+        
+        stopMotors();
+        analogWrite(M1F, 250);
+        delay(1500);
 
+        stopMotors();
+        analogWrite(M1R, 250);
+        delay(1500);
+
+        // analogWrite(M2F, LOW);
+        // analogWrite(M2R, LOW);
+        
         vTaskDelay(taskTesting.getIntervalms() / portTICK_PERIOD_MS);  // Delay for the blink time
     }
 
+}
+
+void stopMotors() {
+    analogWrite(M1F, LOW);
+    analogWrite(M1R, LOW);
+    analogWrite(M2F, LOW);
+    analogWrite(M2R, LOW);
+    delay(1500);
 }
